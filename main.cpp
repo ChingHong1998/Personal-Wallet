87588a43611b48b280d1545792f6adb1a20cda46
ChingHong1998 <39666842+ChingHong1998@users.noreply.github.com>
2018-08-13 20:35:37 +0800

    First Updated

--- main.cpp
+++ main.cpp
@@ -1,13 +1,60 @@
 #include<iostream>
+#include<cstdlib>
+#include <fstream>
 #include "wallet.cpp"
 
-int main() {
-    Wallet w1;
-    cout<< "Welcome!\nEnter 1 to add expense.";
-    int option;
-    cin >> option;
-    if(option == 1) {
-        w1.addExpense();
-    }
+void display_menu();
 
+Wallet *w1;
+
+bool is_emptyf(ifstream& pFile)
+{
+    return pFile.peek() == ifstream::traits_type::eof();
+}
+
+int main() {
+    ifstream fin;
+    fin.open("Wallet.txt",ios::in);
+    if(is_emptyf(fin)) {
+        ofstream fout;
+        fout.open("Wallet.txt",ios::app);
+        cout<< "Create a wallet.\nEnter your username: ";
+        string username;
+        cin >> username;
+        cout << "Enter your balance in your wallet.";
+        double balance;
+        cin>> balance;
+        w1 = new Wallet(username,balance);
+        fout << "Username: "<< username <<endl;
+        fout << "Balance: " << balance <<endl;
+    }
+    else {
+        while(!fin.eof()&& (fin.good()))
+        {
+            string username;
+            double balance;
+            fin >> username;
+            string temp;
+            fin >> temp;
+            stringstream ss(temp);
+            ss >> balance;
+            w1 = new Wallet(username,balance);
+        }
+    }
+    display_menu();
+}
+
+void display_menu() {
+    system("CLS");
+    cout << "Welcome to Personal Wallet.\n"<<w1->displayUsername()<<" current balance is "<< w1->displayBalance() <<endl;
+    cout << "Menu: \n 1. Add Expense. \n 2. Add Income \n ";
+    int option;
+    cin>> option;
+    switch(option) {
+    case 1:
+        w1->addExpense();
+        break;
+    default:
+        cout << "\nInvalid option."<<endl;
+    }
 }
