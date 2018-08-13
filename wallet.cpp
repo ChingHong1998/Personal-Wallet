ee13ff3b502718077a8552d68628802920c9e7b3
ChingHong1998 <39666842+ChingHong1998@users.noreply.github.com>
2018-08-13 20:36:43 +0800

    First Updated

--- wallet.cpp
+++ wallet.cpp
@@ -1,12 +1,17 @@
 #include<iostream>
 #include "wallet.hpp"
 #include "expense.cpp"
+#include "income.cpp"
 
 using namespace std;
 
 /* Can create a saving wallet*/
 
-Wallet::Wallet() : balance(0) {}
+Wallet::Wallet(string name, double bal) : username(name),balance(bal) {
+    cout << "User: " << username <<endl;
+    cout<< "Your current balance: "<< balance<<endl;
+}
+
 Wallet::~Wallet() {}
 
 void Wallet::addExpense(){
@@ -24,14 +29,32 @@ void Wallet::addExpense(){
     cout << "\nAdded!";
 }
 
-void Wallet::addIncome(float Amount,  string Date, string Detail, int Category){}
+void Wallet::addIncome(){
+    Income * income = new Income;
+    cout<<"Enter your amount: ";
+    double amount;
+    cin >> amount;
+    cout<<"Enter the detail: ";
+    string detail;
+    cin >> detail;
+    cout<<"Enter your category: ";
+    string category;
+    cin >> category;
+    income->add_record(amount,detail,category);
+    cout << "\nAdded!";
+}
 void Wallet::deleteExpense(string Date){}
 void Wallet::deleteIncome(string Date){}
 void Wallet::editExpense(string Date){}
 void Wallet::editIncome(string Date){}
 void Wallet::search(string Date){}
 void Wallet::sort(){}
-void Wallet::displayBalance(){}
+double Wallet::displayBalance(){
+    return balance;
+}
+string Wallet::displayUsername() {
+    return username;
+}
 void Wallet::displaySummary(){}
 void Wallet::warning(){}
 void Wallet::displayChart(){}
